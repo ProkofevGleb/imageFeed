201f61e228d062f69d5ae3b73fec5adcac8cc8d6
GlebProkofev <prokofyevg@yandex.ru>
2024-12-01 23:05:35 +0300

    Настройка таблицы и ячеек

--- imageFeed/ImagesList/ImagesListCell.swift
+++ imageFeed/ImagesList/ImagesListCell.swift
@@ -2,4 +2,64 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    // Создаем градиентный слой
+    let gradientLayer = CAGradientLayer()
+    
+    @IBOutlet var cellView: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeLabel: UIButton!
+    
+    override func awakeFromNib() {
+            super.awakeFromNib()
+            addGradientToCellView()
+        }
+    
+    private func addGradientToCellView() {
+        // Устанавливаем размеры градиентного слоя
+        gradientLayer.frame = cellView.bounds
+        
+        // Определяем цвета градиента с использованием HEX-кодов
+        let topColor = UIColor.clear.cgColor // Прозрачный цвет сверху
+        let bottomColor = UIColor(hex: "#1A1B22", alpha: 0.2)?.cgColor // 20% от #1A1B22
+        
+        gradientLayer.colors = [topColor, bottomColor].compactMap { $0 } // Убираем nil значения, если bottomColor не удалось создать
+        
+        // Устанавливаем точки начала и конца градиента
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0) // Начинаем с верхней середины
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1) // Заканчиваем в нижней середине
+        
+        // Устанавливаем местоположения цветов для градиента
+        let height = cellView.bounds.height
+        gradientLayer.locations = [NSNumber(value: (height - 30) / height), NSNumber(value: height / height)]
+        
+        // Добавляем градиентный слой к cellView
+        cellView.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func layoutSubviews() {
+            super.layoutSubviews()
+            // Обновляем фрейм градиента на размер contentView
+            gradientLayer.frame = contentView.bounds
+            
+            // Устанавливаем местоположения для градиента
+            let height = contentView.bounds.height
+            gradientLayer.locations = [NSNumber(value: (height - 30) / height), NSNumber(value: height / height)]
+        }
 }
+
+// Расширение для UIColor для создания цвета из HEX
+extension UIColor {
+    convenience init?(hex: String, alpha: CGFloat = 1.0) {
+        var rgb: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&rgb)
+        
+        let red = CGFloat((rgb >> 16) & 0xFF) / 255.0
+        let green = CGFloat((rgb >> 8) & 0xFF) / 255.0
+        let blue = CGFloat(rgb & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
+
--- imageFeed/ImagesList/ImagesListViewController.swift
+++ imageFeed/ImagesList/ImagesListViewController.swift
@@ -3,33 +3,72 @@ import UIKit
 class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
-    
-    func configCell(for cell: ImagesListCell) { }
 
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-            
-            guard let imageListCell = cell as? ImagesListCell else {
-                return UITableViewCell()
-            }
-            
-            configCell(for: imageListCell)
-            return imageListCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
         }
+        
+//        imageListCell.addGradientToCellView()
+        
+        configCell(for: imageListCell, with: indexPath)
+        return imageListCell
+    }
 }
 
-extension ImagesListViewController: UITableViewDelegate {
-    
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        // Получаем имя изображения по индексу
+        let photoName = photosName[indexPath.row]
+        let currentDate = Date().dateTimeString
+        
+        
+        guard let image = UIImage(named: photoName) else {
+            print("Не удалось создать UIImage с именем \(photoName)")
+            return
+        }
+        
+//        cell.addGradientToCellView()
+        cell.cellView.image = image
+        cell.dateLabel.text = currentDate
+        
+        indexPath.row % 2 == 0
+            ? cell.likeLabel.setImage(UIImage(named: "ActiveLike"), for: .normal)
+            : cell.likeLabel.setImage(UIImage(named: "NoActiveLike"), for: .normal)
+        
+    }
 }
+
+//extension ImagesListViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        guard let image = UIImage(named: photosName[indexPath.row]) else {
+//            return 0
+//        }
+//        
+//        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+//        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+//        let imageWidth = image.size.width
+//        let scale = imageViewWidth / imageWidth
+//        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+//        return cellHeight
+//    }
+//}
